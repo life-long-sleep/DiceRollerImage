f32e61df1ddd8ba8e6486d794b216500bcf91f9e
Shu Yun <limsy-jm18@student.tarc.edu.my>
2021-06-24 16:45:47 +0800

    Practical 1.3

---
+++ app/src/main/java/com/mad/dicerollerimage/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.mad.dicerollerimage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnRoll : Button = findViewById(R.id.roll_button)
+
+        btnRoll.setOnClickListener(){
+            rollDice()
+        }
+
+    }
+
+    private fun rollDice(){
+        var randomInt = (1..6).random()
+        diceImage = findViewById(R.id.dice_image)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+}
